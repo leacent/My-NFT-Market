26c60ed7ee72cb494e75d351673ed4f456b16562
leacent <leason316@foxmail.com>
2024-03-11 14:41:03 +0800

    feat: fund me contract

--- webapp/src/router/index.js
+++ webapp/src/router/index.js
@@ -1,16 +1,30 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from 'pages/Home'
-import Create from 'pages/Create'
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "pages/home";
+import Merchant from "pages/merchant";
+import Buyer from "pages/buyer";
+import Market from "pages/market";
 
-const routes = createBrowserRouter([
-  {
-    path: "/",
-    Component: Home
-  },
-  {
-    path: "/create",
-    Component: Create
-  },
-]);
+const RoutesApp = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/merchant" element={<Merchant />} />
+            <Route path="/buyer" element={<Buyer />} />
+            <Route path="/market" element={<Market />} />
+            <Route path="*" element={<NoMatch />} />
+        </Routes>
+    );
+};
 
-export default routes
+function NoMatch() {
+    return (
+        <div>
+            <h2>Nothing to see here!</h2>
+            <p>
+                <Link to="/">Go to the home page</Link>
+            </p>
+        </div>
+    );
+}
+
+export default RoutesApp;
